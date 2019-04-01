86574d139d0761c7d2845dcee1ae5730db1b5d42
Matt Kleven <klevennb115@gmail.com>
2019-04-01 16:17:31 -0500

    Learning methods and special characters

--- Program.cs
+++ Program.cs
@@ -4,12 +4,34 @@ namespace NumbersInCSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
-            int a = 6;
-            int b = -3;
+            int a = 4;
+            int b = 2;
             int c = a + b;
-            Console.WriteLine(c);
+            int d = 19;
+            int e = 21;
+            Console.WriteLine(a/b+c*d-e);
+            Console.WriteLine(a / b + c);
+            Console.WriteLine(d - e);
+        }
+        static void WorkingWithSymbols()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+        }
+        static void Main (string[] args)
+        {
+            WorkingWithIntegers();
+            WorkingWithSymbols();
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
         }
     }
 }
