71ba6d40746554e88fee5a31f538466cd6037fbd
Matt Kleven <klevennb115@gmail.com>
2019-04-01 15:39:02 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = -3;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
